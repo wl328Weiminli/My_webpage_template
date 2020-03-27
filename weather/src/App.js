d5d40e402ffca84d42a9059eb0fa6270f942d1b9
derek <wl328@njit.edu>
2020-03-27 00:56:44 -0400

    update react component

---
+++ weather/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import WeatherCard from './components/WeatherCard/component';
+
+function App() {
+  return (
+    <div className="App">
+    <WeatherCard />
+    </div>
+  );
+}
+
+export default App;
---
+++ weather/src/components/WeatherCard/component.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const WeatherCard = (props) => {
+    return ( 
+        <div className="card">
+            <div className="location">
+                <h1 className='city'>Sydney</h1>
+                <h3 className='country'>AU</h3>
+            </div>
+            <img className='icon' src="./img/cloudy.png" alt="cloudy"/>
+            <h1 className='temp'>20 ˚C</h1>
+            <h3 className='condition'>Clouds</h3>
+        </div>
+     );
+}
+ 
+export default WeatherCard;
