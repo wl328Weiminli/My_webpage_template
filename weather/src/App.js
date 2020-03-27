d31992c5e1f3e6f7a47daae22a16b9d993d61522
derek <wl328@njit.edu>
2020-03-27 18:59:44 -0400

    format

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import './App.css';
-import WeatherCard from './components/WeatherCard/component';
+import React from "react";
+import "./App.css";
+import WeatherCard from "./components/WeatherCard/component";
 
 function App() {
   return (
     <div className="App">
-    <WeatherCard />
+      <WeatherCard />
     </div>
   );
 }
--- weather/src/components/WeatherCard/component.js
+++ weather/src/components/WeatherCard/component.js
@@ -1,17 +1,17 @@
-import React from 'react';
+import React from "react";
 
-const WeatherCard = (props) => {
-    return ( 
-        <div className="card">
-            <div className="location">
-                <h1 className='city'>Sydney</h1>
-                <h3 className='country'>AU</h3>
-            </div>
-            <img className='icon' src="./img/cloudy.png" alt="cloudy"/>
-            <h1 className='temp'>20 ˚C</h1>
-            <h3 className='condition'>Clouds</h3>
-        </div>
-     );
-}
- 
+const WeatherCard = props => {
+  return (
+    <div className="card">
+      <div className="location">
+        <h1 className="city">Sydney</h1>
+        <h3 className="country">AU</h3>
+      </div>
+      <img className="icon" src="./img/cloudy.png" alt="cloudy" />
+      <h1 className="temp">20 ˚C</h1>
+      <h3 className="condition">Clouds</h3>
+    </div>
+  );
+};
+
 export default WeatherCard;
